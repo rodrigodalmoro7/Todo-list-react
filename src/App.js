a880a209ad3e8aa5b6f627372879ab11d3b058a6
forteshiago <forteshiago@hotmail.com>
2020-08-27 08:36:28 -0300

    Create basic Header

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";   
+import Header from "./components/Header";
+
+function App(){
+    return(
+        <Header title="To Do List - ReactJS App" />
+    )
+}
+
+export default App;
