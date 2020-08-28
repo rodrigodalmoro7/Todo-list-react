663820bb9c8564c8ac3cf81cda23e6e0aba6278b
forteshiago <forteshiago@hotmail.com>
2020-08-27 21:26:58 -0300

    Create Body.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";   
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App(){
     return(
-        <Header title="To Do List - ReactJS App" />
+        <>
+            <Header title="To Do List - ReactJS App" />
+            <Body />
+        </>
     )
 }
 
---
+++ src/components/Body.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Body(){
+
+    return <h1>Aqui vai o codigo</h1>
+}
+
+export default Body;
