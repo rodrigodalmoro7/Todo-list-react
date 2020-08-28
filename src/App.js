ed5f36fe9899f55ec5ce85846920a2e152dc1e82
forteshiago <forteshiago@hotmail.com>
2020-08-28 01:09:47 -0300

    Create simple logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 import "./App.css";
+import { useState } from "react";
 
 function App(){
     return(
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Body(){
 
+    const [tasks, setTasks] = useState([{newTask:"Estudar React", owner:"Luigi", simpleId:"testeUm1010"}]);
+    const [newTask, setNewTask] = useState(""); //string vazia
+    const [owner, setOwner] = useState("");
+
+    function addNewTask() {
+        const task = { newTask, owner, simpleId: `${Date.now()}`};
+  
+        setTasks([...tasks, task]);
+        // console.log(tasks);
+        setNewTask("");
+        setOwner("");
+    }
+
     return (
-        <>
-            <input type="text" placeholder="Digite uma tarefa" />
-            <button>Adicionar!</button>
+        <>  
+            <input value={newTask} placeholder="Digite uma nova tarefa!" type="text" onChange={(e) => setNewTask(e.target.value)} />
+            <br />
+            <input value={owner} placeholder="Digite o nome do criador!" type="text" onChange={(e) => setOwner(e.target.value)} />
+            <br />
+            <button type="button" onClick={addNewTask}>
+                Criar!
+            </button>
+            <br />
+            
+            <h3>{newTask}</h3>
+            <h3>{owner}</h3>
+
+            <ul>
+                { tasks.map((eachTask) => (
+                
+                    <li key={eachTask.simpleId}>
+                        <b>{ eachTask.owner }: </b>{ eachTask.newTask }.
+                    </li>
+                
+                ))}
+            </ul>
         </>
     )
 }
